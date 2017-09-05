37ef7527aa3175b6e1242b792246eca04d612667
Axel Hodler <axel@hodler.co>
2017-09-05 23:46:38 +0200

    can populate xls template

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lessons learned
+
+* File Format needs to be Excel 97-2004 Workbook
+* Add a login library to get proper debug infos if something goes wrong with JXLS
+* Without the POI dependency we get `ArrayIndexOutOfBoundsException`
--- src/main/java/co/hodler/Main.java
+++ src/main/java/co/hodler/Main.java
@@ -1,9 +1,28 @@
 package co.hodler;
 
+import org.jxls.common.Context;
+import org.jxls.util.JxlsHelper;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-  public static void main(String[] args) {
-    // Shows warning and longer dialog during import
-    ExcelFile excelFile = new ExcelFile();
-    excelFile.storeFromString("topLeft\ttopRight\nbotLeft\tbotRight");
+  public static void main(String[] args) throws Exception {
+    List<Person> persons = new ArrayList<>();
+    persons.add(new Person("Pete", 32));
+    persons.add(new Person("Maggie", 33));
+    persons.add(new Person("Allie", 35));
+    try (InputStream is = Files.newInputStream(Paths.get("template.xls"))) {
+      try (OutputStream os = new FileOutputStream("object_collection_output.xls")) {
+        Context context = new Context();
+        context.putVar("persons", persons);
+        JxlsHelper.getInstance().processTemplate(is, os, context);
+      }
+    }
   }
 }
