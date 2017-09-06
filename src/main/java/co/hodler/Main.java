2b285f2aecaf26748bee499bec7f63edefe2032f
Axel Hodler <axel@hodler.co>
2017-09-06 11:54:39 +0200

    can read xls contents

--- src/main/java/co/hodler/Main.java
+++ src/main/java/co/hodler/Main.java
@@ -1,8 +1,11 @@
 package co.hodler;
 
+import jxl.Sheet;
+import jxl.Workbook;
 import org.jxls.common.Context;
 import org.jxls.util.JxlsHelper;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,17 +15,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+  private static final String CREATED_REPORT = "template_output.xls";
+
   public static void main(String[] args) throws Exception {
     List<Person> persons = new ArrayList<>();
     persons.add(new Person("Pete", 32));
     persons.add(new Person("Maggie", 33));
     persons.add(new Person("Allie", 35));
     try (InputStream is = Files.newInputStream(Paths.get("template.xls"))) {
-      try (OutputStream os = new FileOutputStream("object_collection_output.xls")) {
+      try (OutputStream os = new FileOutputStream(CREATED_REPORT)) {
         Context context = new Context();
         context.putVar("persons", persons);
         JxlsHelper.getInstance().processTemplate(is, os, context);
       }
     }
+
+    Workbook workbook = Workbook.getWorkbook(new File(CREATED_REPORT));
+    Sheet sheet = workbook.getSheet(0);
+    List<Person> parsedPersons = new ArrayList<>();
+    int firstRowWithContent = 3;
+    for (int i = firstRowWithContent; i < sheet.getRows(); i++) {
+      Person person = new Person();
+      person.setName(sheet.getCell(0, i).getContents());
+      person.setAge(Integer.valueOf(sheet.getCell(1, i).getContents()));
+      parsedPersons.add(person);
+    }
+    System.out.println("Parsed persons equal the stored persons?");
+    System.out.println(persons.equals(parsedPersons));
   }
 }
--- src/main/java/co/hodler/Person.java
+++ src/main/java/co/hodler/Person.java
@@ -28,4 +28,30 @@ public class Person {
   public int getAge() {
     return age;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Person person = (Person) o;
+
+    if (age != person.age) return false;
+    return name != null ? name.equals(person.name) : person.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = name != null ? name.hashCode() : 0;
+    result = 31 * result + age;
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+      "name='" + name + '\'' +
+      ", age=" + age +
+      '}';
+  }
 }
